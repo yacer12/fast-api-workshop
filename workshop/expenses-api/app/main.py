c1a3931eea75beed09c051954b220f9a2760e23d
yacer12 <yaser.castillo@datasite.com>
2025-04-09 13:04:03 -0600

    Updates to schemas for workshop

--- workshop/expenses-api/app/main.py
+++ workshop/expenses-api/app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from datetime import datetime
+from fastapi import HTTPException
+
+from fastapi import FastAPI, Request
+from starlette.responses import JSONResponse
+from utils.get_client_ip import get_client_ip
+from models.errors import ErrorResponse
+
 
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
-
-# Include routers from different modules
 
